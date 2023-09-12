29871cb94f82dc6d0b9fd363319875cd20c5c7fa
Adrià López Gurrea <82399947+Adrialopezgurrea@users.noreply.github.com>
2023-09-12 05:11:41 +0200

    Update MediaDosisPorPacienteySerie.py

--- MediaDosisPorPacienteySerie.py
+++ MediaDosisPorPacienteySerie.py
@@ -31,10 +31,10 @@ def calculate_average_ctdi_vol(directory_path):
                     ctdi_values = []  # Almacena los valores de CTDIvol para la serie
                     
                     # Itera a través de los archivos en la serie
-                    for root, dirs, files in os.walk(series_dir_path):
-                        for file in files:
-                            if file.lower().endswith(".dcm"):
-                                dicom_file_path = os.path.join(root, file)
+                    for file in os.listdir(series_dir_path):
+                            
+                            if file.lower().endswith(".dcm"): #Comprueba que ele archivo tenga formato DICOM
+                                dicom_file_path = os.path.join(series_dir_path, file)
                                 ctdi_vol = get_ctdi_vol_from_dicom(dicom_file_path)
                                 
                                 # Si se encuentra el valor de CTDIvol, agrégalo a la lista
@@ -44,17 +44,21 @@ def calculate_average_ctdi_vol(directory_path):
                     if ctdi_values:
                         # Calcula la media de los valores de CTDIvol para la serie y paciente
                         series_average = sum(ctdi_values) / len(ctdi_values)
-                        print(f"Media de CTDIvol para paciente {patient_dir}, serie {series_dir} del equipo Somatom Go Top: {series_average}")
+                        print(f"Media de CTDIvol para paciente {patient_dir}, serie {series_dir} del equipo "Nombre_Equipo": {series_average}")
                         patient_ctdi[patient_dir].extend(ctdi_values)
     
     # Calcula y muestra las medias de CTDIvol por paciente
     for patient_id, patient_values in patient_ctdi.items():
         patient_average = sum(patient_values) / len(patient_values)
-        print(f"Media de CTDIvol para paciente {patient_id} del equipo Somatom Go Top: {patient_average}")
+        print(f"Media de CTDIvol para paciente {patient_id} del equipo "Nombre_Equipo": {patient_average}")
 
 if __name__ == "__main__":
-    directory_path = "Ruta/de/la/carpeta/con/imagenes/DICOM"
+    directory_path = "Ruta/Carpeta/Imagenes/DICOM"
     calculate_average_ctdi_vol(directory_path)
+
+
+
+
 
 
 
